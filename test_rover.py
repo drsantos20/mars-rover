a571143f9e9d3ce2b5e774bc648374f21ae4e50f
Daniel Santos <drsantos20@gmail.com>
2018-04-12 08:20:41 -0300

    files has been addeded

---
+++ test_rover.py
@@ -0,0 +1,29 @@
+import rover
+import unittest
+r = rover.rover()
+
+class TestSequenceFunctions(unittest.TestCase):
+
+
+    def test_x_coordinate(self):
+        global r
+        r.set_data([5, 5], 1, 2, 'N', 'LMLMLMLMM')
+        r.coordinates_follow()
+        results = r.get_result()
+        self.assertEqual(results[0], 1)
+    def test_y_coordinate(self):
+        global r
+        r.set_data([5, 5], 3, 3, 'E', 'MMRMMRMRRM')
+        r.coordinates_follow()
+        results = r.get_result()
+        self.assertEqual(results[1], 1)
+    def test_direction(self):
+        global r
+        r.set_data([6, 6], 2, 4, 'S', 'MRMLMMRLMRM')
+        r.coordinates_follow()
+        results = r.get_result()
+        self.assertEqual(results[2], 'W')
+
+        
+if __name__ == '__main__':
+    unittest.main()
